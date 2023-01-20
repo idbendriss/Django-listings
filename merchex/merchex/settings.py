ee75740426fd8181e22cdd3d023100cdd9827e21
Brahim Idbendriss <brahim_idbendriss@yahoo.com>
2023-01-20 21:52:08 +0100

    first

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'merchex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-listings',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -122,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
